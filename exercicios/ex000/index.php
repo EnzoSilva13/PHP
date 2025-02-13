99d8efdde32ae9cfb9a27e9490f8d2e07f0b8c8a
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-13 16:13:01 -0300

    primeiros exercicios

---
+++ exercicios/ex000/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldiçao</title>
+</head>
+<body>
+    <h1>
+        vamos tentar nos livrar das maldiçoes
+
+    </h1>
+    <p>
+        <?php 
+        echo "Ola,Mundo \u{1F30E}" ?>
+    </p>
+    <p> <?php 
+        print 
+        
+        "ola,mundo \u{1F30E}" ?></p>
+    
+</body>
+</html>
