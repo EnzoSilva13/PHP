fcfe2517b93e554d2c9e59fa66b3260ce87ca3a7
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-14 16:16:33 -0300

    Create index.php

---
+++ exercicios/ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e constantes</title>
+</head>
+<body>
+    <h1>Variaveis e Consatntes</h1>
+    <?php 
+    $nome = "enzo";
+    $sobrenome = "donizetti";
+    const PAIS= "BraSil";
+    
+    echo "muito prazer, $nome $sobrenome, voce mora no " .PAIS; ?>
+    
+</body>
+</html>
