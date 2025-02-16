5e672b941a6535ba41f70eba26972e6905540ae1
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-16 14:43:13 -0300

    atualizaçoes nos arquivos php

--- exercicios/ex002/index.php
+++ exercicios/ex002/index.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Variaveis e constantes</title>
+    <title>PHP</title>
 </head>
 <body>
-    <h1>Variaveis e Consatntes</h1>
-    <?php 
-    $nome = "enzo";
-    $sobrenome = "donizetti";
-    const PAIS= "BraSil";
+    <h1>Exemplos de PHP</h1>
+
+        <?php 
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "hoje é dia " .date("d/M/Y");
+        echo " e a hora tual é" .date("G:i:s T");?>
+        
     
-    echo "muito prazer, $nome $sobrenome, voce mora no " .PAIS; ?>
+         
+
     
 </body>
 </html>
