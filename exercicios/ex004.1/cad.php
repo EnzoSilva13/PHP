2d007a514140689275627cc7455aa195da3537f2
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-25 16:34:10 -0300

    Create cad.php

---
+++ exercicios/ex004.1/cad.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores e funçoes aritmetica</title>
+</head>
+<body>
+    <h1>Exercicio de casa para funçoes e operadores aritméticas</h1>
+        
+
+    <?php 
+        
+        echo "<h2> Operadores </h2>";
+        $num = 45 + 5;
+        $num2 = 45 - 5;
+        $num3 = 45 * 2;
+        $num4 = 45 / 2;
+        $num5 = 45 ** 2;
+        $num6 = 45 * (2+2);
+
+        echo "<p> abaixo temos alguns exemplos de operadores: <br><br>
+        ADIÇÃO: 45 + 5 = $num <br><br>
+        SUBTRAÇÃO: 45 - 5 = $num2 <br><br>
+        MULTIPLICAÇÃO: 45 * 2 = $num3 <br><br>
+        DIVISÃO: 45 / 2 = $num4  <br><br>
+        POTENCIA: 45 ** 2 = $num5 <br><br>
+        COM PARENTESES 45 * (2+2) $num6 <br><br>"
+
+    ?>
+    
+</body>
+</html>
